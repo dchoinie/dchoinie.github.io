ac79e42206ecc3bc986186cb84d04dece93cc343
Dan Choiniere <dchoinie@gmail.com>
2018-09-25 10:51:35 -0500

    Recreated Contact Form
    Added contact form, attempted php form handling.

--- contact.php
+++ contact.php
@@ -1,10 +1,46 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "dchoinie@gmail.com";
-$subject = "Website Form Submission";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+
+	$email_from = "dchoinie.github.io";
+	$email_subject = "Website Form Submission";
+	$email_body = "New message from $name.\n". "Message: \n $message".
+
+	$to = "dchoinie@gmail.com";
+	$headers = "From: $email_from \r\n";
+	$headers .= "Reply-to: $email \r\n";
+
+	mail($to,$email_subject,$email_body,$headers);
+
+	function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
 ?>
+
